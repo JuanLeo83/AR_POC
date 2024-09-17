c5a3f70077b143bbcb42ff4dab12f7a83e307b30
Juan Leo <juanleodev@gmail.com>
2024-09-17 18:52:53 +0200

    Project created

---
+++ app/src/main/java/com/capgemini/ar_poc/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.capgemini.ar_poc
+
+import android.os.Bundle
+import android.view.SurfaceView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.capgemini.ar_poc.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+//    var surfaceView: SurfaceView? = null
+    var customViewer: CustomViewer = CustomViewer()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        customViewer.run {
+            loadEntity()
+            setSurfaceView(requireNotNull(binding.surface))
+
+            //directory and model each as param
+//            loadGlb(this@MainActivity, "grogu", "grogu")
+            loadGltf(this@MainActivity, "misc", "adamHead");
+
+            //directory and model as one
+            //loadGlb(this@MainActivity, "grogu/grogu");
+
+            //Enviroments and Lightning (OPTIONAL)
+            loadIndirectLight(this@MainActivity, "venetian_crossroads_2k")
+            //loadEnviroment(this@MainActivity, "venetian_crossroads_2k");
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        customViewer.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        customViewer.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        customViewer.onDestroy()
+    }
+
+}
